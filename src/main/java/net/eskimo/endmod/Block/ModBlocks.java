86df5b71bf234c063cf132d8c75856e5071a5c2c
Eskil <eskil.svantesson@gmail.com>
2025-03-20 16:12:09 +0100

    Initial commit

---
+++ src/main/java/net/eskimo/endmod/Block/ModBlocks.java
@@ -0,0 +1,15 @@
+package net.eskimo.endmod.Block;
+
+import net.eskimo.endmod.EndMod;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, EndMod.MOD_ID);
+
+    public static final RegistryObject<Block> BLOCK_OF_SMILE = registerblock("block_of_smile", 
+            )
+}
